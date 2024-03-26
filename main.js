0cc142a5626409bb0cb18adfbdb2ae4ff500f316
Julien Heng <julienheng0@gmail.com>
2024-03-26 14:36:03 +0100

    Finish clock project

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const clock = () => {
+    const secondsDom = document.querySelector('.seconds');
+    const minutesDom = document.querySelector('.minutes');
+    const hoursDom = document.querySelector('.hours');
+    
+    const date = new Date();
+    console.log(date);
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    console.log(minutes);
+    const seconds = date.getSeconds();
+    
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+    
+    secondsDom.style.transform = `rotateZ(${second}deg)`;
+    minutesDom.style.transform = `rotateZ(${minute}deg)`;
+    hoursDom.style.transform = `rotateZ(${hour}deg)`;
+}
+
+clock();
+
+setTimeout(() => clock(), 1000);
